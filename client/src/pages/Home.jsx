1340aafcfc8ea977d1bd4278186828f58f2e7a34
N S <kwb020312@naver.com>
2023-04-16 22:55:33 +0900

    폰트 적용

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,8 +8,32 @@ import {
   slideAnimation,
 } from "../config/motion";
 
+import state from "../store";
+
 const Home = () => {
-  return <div>Home</div>;
+  const snap = useSnapshot(state);
+  return (
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className="home" {...slideAnimation("left")}>
+          <motion.header {...slideAnimation("down")}>
+            <img
+              src="./threejs.png"
+              alt="logo"
+              className="w-8 h-8 object-contain"
+            />
+          </motion.header>
+          <motion.div className="home-content" {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className="head-text">
+                자, <br className="xl:block hidden" /> 해봅시다!
+              </h1>
+            </motion.div>
+          </motion.div>
+        </motion.section>
+      )}
+    </AnimatePresence>
+  );
 };
 
 export default Home;
