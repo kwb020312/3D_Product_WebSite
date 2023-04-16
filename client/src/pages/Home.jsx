431b04f0ea7716df6df228d8a9fa40dad008790e
N S <kwb020312@naver.com>
2023-04-16 23:11:01 +0900

    홈 페이지

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,6 +9,7 @@ import {
 } from "../config/motion";
 
 import state from "../store";
+import { CustomButton } from "../components";
 
 const Home = () => {
   const snap = useSnapshot(state);
@@ -28,6 +29,25 @@ const Home = () => {
               <h1 className="head-text">
                 자, <br className="xl:block hidden" /> 해봅시다!
               </h1>
+            </motion.div>
+            <motion.div
+              {...headContentAnimation}
+              className="flex flex-col gap-5"
+            >
+              <p className="max-w-md font-normal text-gray-600 text-base">
+                새롭게 만들어진 사용자 도구를 활용하여 자신만의 독특하고 특별한
+                셔츠를 제작할 수 있습니다.{" "}
+                <strong>
+                  {" "}
+                  상상력을 발휘하여 자신만의 스타일을 뽐내보세요!
+                </strong>
+              </p>
+              <CustomButton
+                type="filled"
+                title="내 방식대로 꾸미기"
+                handleClick={() => (state.intro = false)}
+                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+              />
             </motion.div>
           </motion.div>
         </motion.section>
