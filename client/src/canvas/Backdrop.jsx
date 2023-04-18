dcb64154149eaea471fe8cb58523bfaf3611232b
N S <kwb020312@naver.com>
2023-04-18 22:02:30 +0900

    카메라 확대

--- client/src/canvas/Backdrop.jsx
+++ client/src/canvas/Backdrop.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useRef } from "react";
+import { easing } from "maath";
+import { useFrame } from "@react-three/fiber";
+import { AccumulativeShadows, RandomizedLight } from "@react-three/drei";
 
 const Backdrop = () => {
-  return <div>Backdrop</div>;
+  return (
+    <AccumulativeShadows position={[0, 0, -0.14]}>
+      <RandomizedLight amount={4} />
+    </AccumulativeShadows>
+  );
 };
 
 export default Backdrop;
