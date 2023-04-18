cb86fa3e190c16f0f96725dac0cab33f0765555a
N S <kwb020312@naver.com>
2023-04-18 22:08:27 +0900

    Feat-그림자 추가

--- client/src/canvas/Backdrop.jsx
+++ client/src/canvas/Backdrop.jsx
@@ -4,9 +4,24 @@ import { useFrame } from "@react-three/fiber";
 import { AccumulativeShadows, RandomizedLight } from "@react-three/drei";
 
 const Backdrop = () => {
+  const shadows = useRef();
   return (
-    <AccumulativeShadows position={[0, 0, -0.14]}>
-      <RandomizedLight amount={4} />
+    <AccumulativeShadows
+      ref={shadows}
+      temporal
+      frames={60}
+      alphaTest={0.85}
+      scale={10}
+      rotation={[Math.PI / 2, 0, 0]}
+      position={[0, 0, -0.14]}
+    >
+      <RandomizedLight
+        amount={4}
+        radius={9}
+        intensity={0.55}
+        ambient={0.25}
+        position={[5, 5, -10]}
+      />
     </AccumulativeShadows>
   );
 };
