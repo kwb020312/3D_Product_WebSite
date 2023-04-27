4b84adf2f3a9904c6c4ea6d71f363de59f5557f6
김우빈 <46777310+kwb020312@users.noreply.github.com>
2023-04-27 12:19:52 +0900

    Fix-Base FullTexture Change False

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -4,7 +4,7 @@ const state = proxy({
   intro: true,
   color: "#EFBD48",
   isLogoTexture: true,
-  isFullTexture: true,
+  isFullTexture: false,
   logoDecal: "./threejs.png",
   fullDecal: "./threejs.png",
 });
